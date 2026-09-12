d584a35a0190492839f10b933177d8f0f5e68824
mrryandford-ui <mrryandford@gmail.com>
2026-09-12 14:33:02 -0700

    chore: remove duplicate nested tool directories (it_support/it_support, osint/osint, pentest/pentest)

--- zeropoint/osint/osint/__init__.py
+++
@@ -1,6 +0,0 @@
-﻿"""ZeroPoint OSINT + Investigations sub-package."""
-from zeropoint.osint.pipeline import OSINTPipeline
-from zeropoint.osint.investigations import InvestigationsAgent
-
-__all__ = ["OSINTPipeline", "InvestigationsAgent"]
-
